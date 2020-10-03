72317cecf6765e6dbef1d4efab8d9f7a431e4443
Calcopod <rosca.serban2@gmail.com>
2020-10-03 18:42:02 +0300

    Updates

---
+++ precache-manifest.2e5e8b6a685c4b8b18c4149d9996aa8e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62b0c6f96a309d80e71ca3633093ad2c",
+    "url": "/Chat/index.html"
+  },
+  {
+    "revision": "93148385192ea64c0ae8",
+    "url": "/Chat/static/css/main.98c4adee.chunk.css"
+  },
+  {
+    "revision": "968f956e7c3f65f00f0d",
+    "url": "/Chat/static/js/2.87cb1b83.chunk.js"
+  },
+  {
+    "revision": "1e518d9174bb964e16f207099bfb7407",
+    "url": "/Chat/static/js/2.87cb1b83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93148385192ea64c0ae8",
+    "url": "/Chat/static/js/main.26ab3aa9.chunk.js"
+  },
+  {
+    "revision": "dfbad23f62dac35a3cdd",
+    "url": "/Chat/static/js/runtime-main.53fe9f77.js"
+  }
+]);
